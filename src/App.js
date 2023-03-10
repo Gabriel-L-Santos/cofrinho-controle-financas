75f61632e24b463273c664e1a4e034fc5ae932f1
Gabriel Leme dos Santos <gabriellemesantos@gmail.com>
2023-03-10 20:14:57 -0300

    montando estrutura do projeto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Global from './styles/global';
 
 const App = () => {
     return (
-        <div>App</div>
-    )
-}
+        <>
+            <div>App</div>
+            <Global />
+        </>
+    );   
+};
 
-export default App
+export default App;
