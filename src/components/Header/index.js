4b67324c523be1d2588a6425bb29403d4f447fba
Gabriel Leme dos Santos <gabriellemesantos@gmail.com>
2023-03-11 20:36:48 -0300

    estilizando a página

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import * as C from "./styles";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <C.Container>
+      <C.Header>
+        <C.Title>💲Cofrinho💲</C.Title>
+      </C.Header>
+    </C.Container>
+  );
 };
 
 export default Header;
